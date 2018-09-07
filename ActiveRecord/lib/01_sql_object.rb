7eb0e1a740b87bbe65e22c15a83d4fe490904f77
sirivat <dsirivat17@gmail.com>
2018-09-07 15:35:47 -0700

    Implement getter and setter

--- ActiveRecord/lib/01_sql_object.rb
+++ ActiveRecord/lib/01_sql_object.rb
@@ -53,7 +53,14 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      if self.class.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
   end
 
   def attributes
