52e4f997099e0b0c37eaba22c30edcfff1f8f968
sirivat <dsirivat17@gmail.com>
2018-09-07 14:52:04 -0700

    Update col name

--- ActiveRecord/lib/01_sql_object.rb
+++ ActiveRecord/lib/01_sql_object.rb
@@ -3,9 +3,17 @@ require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
+
 class SQLObject
   def self.columns
     # ...
+    return @columns if @columns
+    cols = DBConnection.execute(<<-SQL)
+      SELECT *
+      FROM #{self.table_name}
+      LIMIT 0
+    SQL
+    @columns = cols
   end
 
   def self.finalize!
@@ -13,14 +21,19 @@ class SQLObject
 
   def self.table_name=(table_name)
     # ...
+    @table_name = table_name
   end
 
   def self.table_name
     # ...
+    @table_name = self.name.tableize
+    @table_name =  'humans' if @table_name = 'humen'
+    @table_name
   end
 
   def self.all
     # ...
+
   end
 
   def self.parse_all(results)
